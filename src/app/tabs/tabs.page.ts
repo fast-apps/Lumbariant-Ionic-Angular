956717c9088fd87d726afa37ad05732fab679f8b
Juan Facundo Cena <jfcena@gmail.com>
2023-12-12 16:40:57 +0100

    updated tab button icons

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,19 +1,27 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonImg } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  selector: 'app-tabs',
+  styleUrl: 'tabs.page.scss',
+  templateUrl: 'tabs.page.html',
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonImg, CommonModule],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
+  isIconChange: boolean = false;
+  isNotHome: boolean = false;
 
   constructor() {
     addIcons({ triangle, ellipse, square });
+  }
+
+  tabClicked(e: any) {
+    e.tab !== 'home' ? this.isIconChange = true : this.isIconChange = false;
+    e.tab !== 'home' ? this.isNotHome = true : this.isNotHome = false;
   }
 }
