cb2bf17cae1c7b78462e9ddc6182a2b1c6d06314
Juan Facundo Cena <jfcena@gmail.com>
2023-12-14 23:37:24 +0100

    Added Ion-progress to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 // Angular
-import { ChangeDetectionStrategy, Component, WritableSignal, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Signal, ViewChild, WritableSignal, computed, signal } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 // Ionic
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonBadge,
@@ -30,9 +30,35 @@ import { DataService } from '../services/data.service';
 })
 export class HomePage {
 
-  source: WritableSignal<any[]> = signal([]);
+  @ViewChild('swiper', { static: false }) 
+  private _swiperRef: ElementRef | undefined;
+
+  // $ignals
+  $source: WritableSignal<any[]> = signal([]);
+  $activeIdx : WritableSignal<number> = signal(0);
+  $progress: Signal<number> = computed(() => (this.$activeIdx()+1) / this.$source().length );
 
   constructor(private dataService: DataService) {
-    this.source.set(this.dataService.getVideoList());
+    this.$source.set(this.dataService.getVideoList());
   }
+
+  ngAfterViewInit() {
+      // now we need to assign all parameters to Swiper element
+    console.log(this._swiperRef);
+
+    this._swiperRef?.nativeElement.setAttribute('slides-per-view', '1');
+ 
+  }
+
+  onSwiperSlideChange(event: any) {
+    // Importante: en swiper element los eventos solo escriben en minúscula!
+    // template: <swiper-container ... (swiperslidechange)="onSwiperSlideChange($event)>
+    this.$activeIdx.set(event.detail[0].activeIndex);
+    // console.log('this.$pregress(): ', this.$activeIdx() / this.$source().length );
+  }
+  
+  onSnapIndexChange(event: any) {
+    console.log('onSnapIndexChange: ', event);
+  }
+
 }
